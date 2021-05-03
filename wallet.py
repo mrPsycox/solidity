44ea8c67e6ed13972e468f8fefd8277c01bc751b
mrpsycox <silvestri.1699926@studenti.uniroma1.it>
2021-05-03 15:28:46 +0200

    wallet python script

--- wallet.py
+++ wallet.py
@@ -10,7 +10,7 @@ balance=w3.fromWei(w3.eth.get_balance(address),'ether')
  print(f"{address} : {balance} ether")
 
 if __name__=="__main__":
-    nodeIp="http://127.0.0.1:7545"
+    nodeIp="http://127.0.0.1:7545"  #infura
     address="bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23"
 
     getBalance(nodeIp,address)
