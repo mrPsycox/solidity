5104450a610f2ec2a52573518ea212455c3e1d2f
mrpsycox <silvestri.1699926@studenti.uniroma1.it>
2021-05-03 14:47:08 +0200

    added notes

--- wallet.py
+++ wallet.py
@@ -1,2 +1,16 @@
-import web3
+from web3 import Web3
 
+def getBalance(nodeIp,address):
+
+ w3=Web3(Web3.HTTPProvider(nodeIp))
+
+ #print(w3.isConnected())
+balance=w3.fromWei(w3.eth.get_balance(address),'ether')
+
+ print(f"{address} : {balance} ether")
+
+if __name__=="__main__":
+    nodeIp="http://127.0.0.1:7545"
+    address="bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23"
+
+    getBalance(nodeIp,address)
